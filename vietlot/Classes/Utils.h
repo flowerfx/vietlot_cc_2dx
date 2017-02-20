8fc15a1e261231b36df94fe105199a5c48a03721
flowerfx <qchien.gl@hotmail.com>
2017-02-20 12:03:34 +0700

    * fix bug

--- vietlot/Classes/Utils.h
+++ vietlot/Classes/Utils.h
@@ -4,82 +4,24 @@
 #include "cocos2d.h"
 #include <string>
 #include <vector>
-#include "Common/Common.h"
 USING_NS_CC;
+
+typedef unsigned char			ubyte;
+typedef unsigned short			u16;
+typedef short					s16;
+typedef unsigned int			u32;
+typedef int						s32;
+typedef unsigned long long		u64;
+typedef long long			    s64;
 
 namespace Utils
 {
-	extern std::vector<s64> s_popular_bet;
-	extern int				s_padding;
-
-	char _toLowerCase(char c);
-
-	std::string toUpperCase(std::string str);
-
-	extern Vec2 s_bounding_offset;
-
-	std::string formatMoney_dot_ruby(s64 ruby);
-	std::string formatMoney_dot_win(s64 money);
-
-	std::string formatNumber_dot(s64 money);
-
-	std::string formatNumber_dot_short(s64 money);
-
-	std::string toLowerCase(std::string str);
-	std::string trim(std::string str);
-
-	s64	getMaxValidBet(s64 value);
-	int	getMaxValidBetIdx(s64 value);
-	int getNumberValidBetIdx();
-	const std::vector<s64> & GetBetLobby();
-
-
-	std::string formatNumber(s64 n);
-	std::string formatNumber_win(s64 n);
-	std::string formatNumber_ruby(s64 n);
-
-	s64 GetNumberFromFormat(std::string format);
-
-	const wchar_t * GetWChar(const char *c);
-	std::string getAvataUrlPlayer(int idUser, std::string userName);
-	void SetImageAvatarLink(void * data);
-
-	int countStringLine(std::string str);
-
-	std::string chatConvert(std::string chat);
-	std::string getEmotionStr(int idx);
-	void		startWaitDialog(std::string content = "");
-	void		hideWaitDialog();
-	void		showDialogNotice(std::string content);
-	void		showToast(std::string content);
-
+	//convert date time
+	void convertToDate(const std::string & date, u32 & day, u32 & month, u32 & year);
+	std::string convertToDate1(s64 date);
+	u32 getDayWeek(const u32 & day, const u32 & month, const u32 & year);
 	//
-	std::vector<s64> GetBetPopularNew();
-	s64 GetBetValueNew(int idx);
-	int GetRangeBetWithMoney(s64 &min_bet, s64 &max_bet, s64 &ave_bet, s64 curent_money , s64 min);
-
-	s64 getMaxValidBetNew(s64 value);
-	int GetIndexOfValue(s64 value);
-	int GetIndexOfValue(std::vector<s64> arrBet,s64 value);
-
-	std::string ConvertTime(s64 time);
-	void replaceAllString(std::string& str, const std::string& from, const std::string& to);
-	std::vector<std::string> split(std::string data, std::string token);
-	
-	void configPadding();
-
-	Vec2 getPositionAtAnchorPoint(Vec2 anchor_point, cocos2d::Node * node);
-
-	const std::string getImgUrlCorrect(const std::string & url);
-
-	//
-	void enableVibrate(bool value);
-	void vibrate(int milisec);
-
-	void vibrateShort();
-	void vibrateVeryShort();
-	void vibrateMedium();
-	void vibrateLong();
+	std::vector<u32> convertToListNumber(const std::string & number);
 
 };
 
